abfc6339ac23339e9e91b4398724ef2cd7b8278f
1kmathews <89388282+1kmathews@users.noreply.github.com>
2021-09-19 19:58:48 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+ if (keyIsDown(DOWN_ARROW))
+ {
+
+}
+
+ if (keyIsDown(UP_ARROW))
+ {
+background('red');
+
+ }
+
+ if (keyIsDown(DOWN_ARROW))
+ {
+background('green');
+
+ }
+
+ if (keyIsDown(LEFT_ARROW))
+ {
+background('blue');
+
+ }
+
+ if (keyIsDown(RIGHT_ARROW))
+ {
+background('yellow');
+
+ }
+}
+
+
+
+
